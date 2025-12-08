23b360d2022129b5299b2f1649fa12edcb0cf234
salimdev04 <salimdev04@gmail.com>
2025-12-08 18:11:22 +0100

    chore: Configure webpack module resolution, fallbacks, and externals, update the build script, and remove unused public assets.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      tap: false,
+      tape: false,
+      "why-is-node-running": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
